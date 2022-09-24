6bba055fba6531e6f7b3ffe03ddd10d6c238f43b
jlbbacs <josephlestebacs@gmail.com>
2022-09-24 10:01:37 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+const App = () => {
+    return ( 
+      <div>
+        
+        <h1> Hello Test </h1> 
+        </div>
+    );
+};
+export default App;
+
+
+
+
+
