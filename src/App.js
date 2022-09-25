1913063ee9beb8ea1998ae41adacad78659dafac
jlbbacs <josephlestebacs@gmail.com>
2022-09-25 10:43:53 +0800

    adding pages

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "./components/nav";
 
+
 const App = () => {
     return ( 
       <Nav/>
+    
+
     );
 };
 export default App;
