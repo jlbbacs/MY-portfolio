9b3129dc5a8112d6c44247c9094299515e3f5bf3
jlbbacs <josephlestebacs@gmail.com>
2022-09-26 11:35:00 +0800

    adding 4 pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "./components/nav";
-import Homepage from"./pages/home";
+import Home from"./pages/home";
 import Abouts from"./pages/about";
 import Contact from"./pages/contact";
 import Works from"./pages/works";
@@ -12,18 +12,16 @@ import {
   NavLink,
 } from "react-router-dom";
 
-
-
 const App = () => {
     return ( 
      
       <Router>
       <Nav/>
       <Routes>
-      <Route path="Homepage/" element={<Homepage />} />
-      <Route path="Abouts/" element={<Abouts />} />
-      <Route path="Contacts/" element={<Contact />} />
-      <Route path="Works/" element={<Works />} />
+      <Route path="/home" element={<Home/>} />
+      <Route path="/about" element={<Abouts />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/works" element={<Works />} />
        
       </Routes>
       
--- src/components/nav.js
+++ src/components/nav.js
@@ -20,7 +20,7 @@ const nav = () => {
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <Link className="nav-link " aria-current="page" to="/home">Home</Link>
+          <Link className="nav-link " to="/home">Home</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/about">Abouts</Link>
