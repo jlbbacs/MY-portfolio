1d57bf41f83a5ab998c9840b66249d1925497bec
jlbbacs <josephlestebacs@gmail.com>
2022-09-27 18:20:10 +0800

    fonts

--- src/pages/home.js
+++ src/pages/home.js
@@ -21,7 +21,7 @@ const home = () => {
      
         <div className='container '>
           <div className='row text-center hero-section'>
-            <div className="col-sm hero-text" ><h4 className='hello' data-aos="fade-right">Hello my name is </h4><span><h1 className='myname' data-aos="fade-left"> JOSEPH LESTER BACSARSA</h1> </span>
+            <div className="col-sm hero-text" id='hero'><h4 className='hello' data-aos="fade-right">Hello my name is </h4><span><h1 className='myname' data-aos="fade-left"> JOSEPH LESTER BACSARSA</h1> </span>
             <h4 className='developer' data-aos="fade-up">And  i'm a Front-end Developer</h4>
              </div>
 
