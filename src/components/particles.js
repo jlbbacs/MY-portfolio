54bd172c5b4dc6a3d890a5664cb4f4d9aabe65b1
jlbbacs <josephlestebacs@gmail.com>
2022-09-29 20:19:34 +0800

    adding form

---
+++ src/components/particles.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function particles() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default particles
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,9 +3,27 @@ import "../css/contact.css";
 
 const contact = () => {
   return (
-    <div>
-       <h1 className='text-center mt-5 Contact'>Contact</h1>
+   <div className='container'>
+      <h1 className='contact-me mt-5'>Contact Me</h1>
+      <form className='form'>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="input">Name</label>
+      <input type="email" class="form-control" id="inputName" placeholder="Name"></input>
     </div>
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Email Address</label>
+      <input type="password" class="form-control" id="inputEmail4" placeholder="Email"></input>
+    </div>
+  </div>
+  <div class="form-group col-md-6">
+    <label for="exampleFormControlTextarea1">Message </label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Message"> </textarea>
+  </div>
+  <button type="button" class="btn btn-outline-primary mt-5">Submit</button>
+  </form>
+  
+  </div>
 
 
 
